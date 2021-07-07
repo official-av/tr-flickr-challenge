8a2d9c98d0952e149a9860ee4c2ea5a1db67a15f
Anmol Vashistha <anmolvashisthaav@gmail.com>
2021-07-07 19:53:46 +0530

    config update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=6ad82343b5f33d55812dc2208820cc2c'
 };
